daa05462a11fe5b0ecf261781dd69b8489b6b72f
Kaio <kaiohtf38@gmail.com>
2025-10-16 15:59:06 -0300

    feat: add chat application

---
+++ src/main/java/br/com/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package br.com.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/br/com/chat/config/WebSocketEventListener.java
@@ -0,0 +1,37 @@
+package br.com.chat.config;
+
+import br.com.chat.chat.ChatMessage;
+import br.com.chat.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplete;
+
+    @EventListener
+    public void handleWebSocketDisconnectListenner(
+            SessionDisconnectEvent event
+    ){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null){
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messageTemplete.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
